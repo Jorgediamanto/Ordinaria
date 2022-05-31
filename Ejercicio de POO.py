3a35d049a192acc7c4959e0182f5ee98ed970bb7
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-05-31 23:39:09 +0200

    Update Ejercicio de POO.py

--- Ejercicio de POO.py
+++ Ejercicio de POO.py
@@ -10,9 +10,9 @@ class UserAccount:
     def __init__(self, alias, email, tweets, followers, timeline):
         self.alias = alias #string {es un nombre}
         self.email = email #string {es un email}
-        self.tweets = tweets #List of strings [time(string)/message(string)/sender(string)]  {cada tweet tiene 3 componentes, por lo que un tweet estará compuesto por 3 string independientes, lo único que en este caso el sender es el mismo}   
+        self.tweets = tweets #List of strings [time(string),message(string),sender(string)]  {cada tweet tiene 3 componentes, por lo que un tweet estará compuesto por 3 string independientes, lo único que en este caso el sender es el mismo}   
         self.followers = followers #List of strings [lista de alias]  {lista de nombres de los seguidores}
-        self.timeline = timeline #List of strings [time(string)/message(string)/sender(string)]   {lo mismo que en los tweets, pero esta vez si hay diferentes senders}
+        self.timeline = timeline #List of strings [time(string),message(string),sender(string)]   {lo mismo que en los tweets, pero esta vez si hay diferentes senders}
         self.usuarios.append(self) 
 
     #este método recibe la instancia del usuario al que desea seguir
