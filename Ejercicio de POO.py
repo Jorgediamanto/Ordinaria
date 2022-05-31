6716739af14a74bbad737e16150c3aea6d11b45c
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-05-31 22:51:38 +0200

    Add files via upload

--- Ejercicio de POO.py
+++ Ejercicio de POO.py
@@ -5,12 +5,15 @@
 
 class UserAccount:
 
+    usuarios = []
+
     def __init__(self, alias, email, tweets, followers, timeline):
         self.alias = alias #string {es un nombre}
         self.email = email #string {es un email}
         self.tweets = tweets #List of strings [time(string)/message(string)/sender(string)]  {cada tweet tiene 3 componentes, por lo que un tweet estará compuesto por 3 string independientes, lo único que en este caso el sender es el mismo}   
         self.followers = followers #List of strings [lista de alias]  {lista de nombres de los seguidores}
         self.timeline = timeline #List of strings [time(string)/message(string)/sender(string)]   {lo mismo que en los tweets, pero esta vez si hay diferentes senders}
+        self.usuarios.append(self) 
 
     #este método recibe la instancia del usuario al que desea seguir
     def follow(self,user):
@@ -29,11 +32,22 @@ class UserAccount:
             print("Este usuario ya sigue a "+user.alias)
         else:
             user.followers.extend([self.alias])
-            user.timeline.extend(self.tweets)
+            self.timeline.extend(user.tweets)
+
 
 
     def tweet(self,tweet):
-        self.tweets.extend(tweet)
+        self.tweets.extend([tweet])
+        #self.usuarios[0].timeline.extend([tweet])
+
+        for x in range(len(self.usuarios)):
+            for y in range(len(self.followers)):
+                if self.usuarios[x].alias == self.followers[y]:
+                    self.usuarios[x].timeline.extend([tweet])
+
+        
+            
+            
 
     def tweet_aux(self):
         pass
@@ -43,23 +57,26 @@ class UserAccount:
 
 
 
-user1 = UserAccount("elvargas","elvarginhos@gmail.com",[["12/10/2001 22:10","Hoy he nacido","elvargas"],["12/10/2091 22:10","Hoy he muerto","elvargas"]],["roberto"],[["31/01/2031 22:10","Adiós","roberto"]])
-user2 = UserAccount("roberto","roberto@yahoo.com",[["31/01/2031 22:10","Adiós","roberto"]],["elvargas"],[["12/10/2001 22:10","Hoy he nacido","elvargas"],["12/10/2091 22:10","Hoy he muerto","elvargas"]])
-user3 = UserAccount("fede","federico@uax.es",[["30/6/2022 22:10","no me sigue nadie D:","fede"]],[],[])
+user1 = UserAccount("user1","elvarginhos@gmail.com",[["12/10/2001 22:10","Hoy he nacido","user1"],["12/10/2091 22:10","Hoy he muerto","user1"]],["user2"],[["31/01/2031 22:10","Adiós","user2"]])
+user2 = UserAccount("user2","roberto@yahoo.com",[["31/01/2031 22:10","Adiós","user2"]],["user1"],[["12/10/2001 22:10","Hoy he nacido","user1"],["12/10/2091 22:10","Hoy he muerto","user1"]])
+user3 = UserAccount("user3","federico@uax.es",[["30/6/2022 22:10","no me sigue nadie D:","user3"]],[],[])
 
 
 
-#user1.follow(user3)
-#print(user3.timeline)
+user1.follow(user3)
 
 
-tweet1 = [["03/04/1990 21:10"],["slavaje "],["fede"]]
+
+tweet1 = ["03/04/1990 21:10","slavaje ","user3"]
 
 user3.tweet(tweet1)
 
-    
-print(user3.tweets)
-    
+
+
+
+#print(user1.usuarios[1].alias)
+#print(user2.usuarios)
+#print(user3.usuarios)
 
 
 
