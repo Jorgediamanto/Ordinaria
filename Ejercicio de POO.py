660e547b5786a29c2b0285af9c8817deb0521474
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-06-01 22:50:41 +0200

    Add files via upload

--- Ejercicio de POO.py
+++ Ejercicio de POO.py
@@ -2,11 +2,11 @@ class UserAccount:
     usuarios = []
 
     def __init__(self, alias, email, tweets, followers, timeline):
-        self.alias = alias
-        self.email = email
-        self.tweets = tweets
-        self.followers = followers
-        self.timeline = timeline
+        self.alias = alias #string {es un nombre}
+        self.email = email #string {es un email}
+        self.tweets = tweets #List of strings [time(string)/message(string)/sender(string)]  {cada tweet tiene 3 componentes, por lo que un tweet estará compuesto por 3 string independientes, lo único que en este caso el sender es el mismo}   
+        self.followers = followers #List of strings [lista de alias]  {lista de nombres de los seguidores}
+        self.timeline = timeline #List of strings [time(string)/message(string)/sender(string)]   {lo mismo que en los tweets, pero esta vez si hay diferentes senders}
         self.usuarios.append(self)
 
     def follow(self, user):
