b3f6d5ae2d263c7b60510dc4a3e4c9f83819b2b0
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-06-02 11:29:14 +0200

    Add files via upload

---
+++ Ejercicio de Ánalisis EDA de Dataset.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+train = pd.read_csv('/kaggle/input/tweet-sentiment-extraction/train.csv')
+test = pd.read_csv('/kaggle/input/tweet-sentiment-extraction/test.csv') 
+ss = pd.read_csv('/kaggle/input/tweet-sentiment-extraction/sample_submission.csv')
+
+
+
